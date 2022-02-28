c076f5b3458558979783995024ca455ff627b45a
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-02-28 20:10:03 +0800

    Add project files.

---
+++ 3Sum/3Sum.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3Sum
+{
+    public class ThreeSum
+    {
+        public static IList<IList<int>> ThreeSumAns(int[] nums)
+        {
+            var answer = new List<IList<int>>();
+            List<IList<int>> anslist = new List<IList<int>>(); //{ new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } };
+
+            if (nums == null || nums.Length == 0)
+            {
+                return answer;
+            }
+            else
+            {
+                Array.Sort(nums);
+                for(int firstNum = 0; firstNum +2 < nums.Length; firstNum++)
+                {
+                    if(firstNum > 0 &&nums[firstNum]== nums[firstNum - 1])
+                    {
+                        break;
+                    }
+                    int secNum = firstNum + 1;
+                    int thrNum = nums.Length - 1;
+                    int target = -nums[firstNum];
+                    
+                    while(secNum < thrNum)
+                    {
+                        if(nums[secNum] + nums[thrNum] == target)
+                        {
+                            IList<int> list = new int[] { nums[firstNum], nums[secNum], nums[thrNum] };
+                            anslist.Add(list);
+                            secNum++;
+                            thrNum--;
+
+                            while(secNum<thrNum && nums[secNum]== nums[secNum - 1])
+                            {
+                                secNum++;
+                            }
+                            while (secNum < thrNum && nums[thrNum] == nums[thrNum + 1])
+                            {
+                                thrNum--;
+                            }
+                        }
+                        else if(nums[secNum] + nums[thrNum] > target)
+                        {
+                            thrNum--;
+                        }
+                        else
+                        {
+                            secNum++;
+                        }
+                    }
+                }
+                return anslist;
+            }
+            
+        }
+    }
+}
---
+++ 3SumTests/ThreeSumTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3Sum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace _3Sum.Tests
+{
+    [TestClass()]
+    public class ThreeSumTests
+    {
+        [TestMethod()]
+        public void ThreeSumAnsTestForNull()
+        {
+            var result = ThreeSum.ThreeSumAns(new int[] { });
+            var expect = new List<IList<int>>();
+            result.Should().BeEquivalentTo(expect, ans => ans.WithStrictOrdering());
+        }
+
+        [TestMethod()]
+        public void ThreeSumAnsTestForZero()
+        {
+            var result = ThreeSum.ThreeSumAns(new int[0]);
+            var expect = new List<IList<int>>();
+            result.Should().BeEquivalentTo(expect, ans => ans.WithStrictOrdering());
+        }
+
+        [TestMethod()]
+        public void ThreeSumAnsTestForNormal()
+        {
+            var result = ThreeSum.ThreeSumAns(new int[6] {-1, 0, 1, 2, -1, -4});
+            var expect = new List<IList<int>>() { new int[] { -1, -1, 2 }, new int[] { -1, 0, 1 } };
+            //IList<int> list1 = new int[] { -1, -1, 2 };
+            //IList<int> list2 = new int[] { -1, 0, 1 };
+            //expect.Add(list1);
+            //expect.Add(list2);
+            result.Should().BeEquivalentTo(expect, ans => ans.WithStrictOrdering());
+        }
+    }
+}
